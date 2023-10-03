7170ceca04b780cc62d702c17383584db4767609
Deepika <dubeydeepika1209@gmail.com>
2023-10-03 13:41:55 +0530

    Completed Animation Part ( no javascript )

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-var num1=5;
+/*var num1=5;
 var num2=10;
 console.log(num1)
 console.log(num2)
 
 var str1="Deepika Dubey";
 var arr1=[ 1,2,3,4,5];
-console.log(arr1)
+console.log(arr1)*/
+// const audio = document.getElementById("song"); // Get the audio element by its ID
 
+// function playAudio() {
+//   audio.play(); // Play the audio
+// }
+
+// function pauseAudio() {
+//   audio.pause(); // Pause the audio
+// }
+
+// function stopAudio() {
+//   audio.pause(); // Pause the audio
+//   audio.currentTime = 0; // Reset the audio to the beginning
+// }
